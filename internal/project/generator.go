e0136808a7d95cd3482786ae5abcfcae907fa90c
Saketh Pai <paisaketh@gmail.com>
2025-10-25 12:21:01 +0530

    style: formatting generator.go

--- internal/project/generator.go
+++ internal/project/generator.go
@@ -8,15 +8,15 @@ import (
 )
 
 type CatalystConfig struct {
-    Project struct {
-        Name    string `yaml:"name"`
-        Created string `yaml:"created"`
-    } `yaml:"project"`
+	Project struct {
+		Name    string `yaml:"name"`
+		Created string `yaml:"created"`
+	} `yaml:"project"`
 
-    Settings struct {
-        Author   string `yaml:"author"`
-        License  string `yaml:"license"`
-    } `yaml:"settings"`
+	Settings struct {
+		Author   string `yaml:"author"`
+		License  string `yaml:"license"`
+	} `yaml:"settings"`
 }
 
 func GenerateYAML(projectName string, authorName string, license string) (string, error) {
